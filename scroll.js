57ff0acd146cb2cf5a4075d20e870169998b6e86
Komaram Praveen Balaji <praveenbalaji0916@gmail.com>
2026-01-25 16:44:38 +0530

    Delete scroll.js

--- scroll.js
+++
@@ -1,9 +0,0 @@
-const bar = document.querySelector(".bottom-bar");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 300) {
-    bar.style.display = "flex";
-  } else {
-    bar.style.display = "none";
-  }
-});
